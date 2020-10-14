f8cf7df23ecbcf17ae5c0e9299511b48c95815ef
Yifan Lin <linyifan1024@yahoo.com>
2020-10-13 22:08:32 -0500

    Update 26. Remove Duplicates from Sorted Array.cpp

--- 26. Remove Duplicates from Sorted Array.cpp
+++ 26. Remove Duplicates from Sorted Array.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
      nums.erase(unique(nums.begin(),nums.end()),nums.end());
+      //unique is to remove duplicates from a list  in a range (first,last)
         
+      //erase is to delete elemnt from a list container (frist, last) or (element position)
         return nums.size();
     }
 };
